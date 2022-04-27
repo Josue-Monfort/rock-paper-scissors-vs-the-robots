a32ed845ca6293f518a7f0b1e0fc2d1f1d67a79d
josue <josuemonfortsantiago@gmail.com>
2022-04-27 11:20:12 -0300

    Rock paper scissors click functionality

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const allHandsButtons = document.querySelectorAll(".buttons") // selects all the rock paper scissors buttons at the bottom
 
+// Adds a click event listener to all buttons
+allHandsButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        console.log(e.target.id)
+    })
+})
 
 // Makes a random choice between rock, paper and scissors
 function getRobotChoice() {
