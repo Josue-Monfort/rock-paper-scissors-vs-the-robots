05ca682aec2c9dbed720a192da036204213b39f6
josue <josuemonfortsantiago@gmail.com>
2022-04-27 11:38:51 -0300

    Added the player and robot won functions. Also the draw function

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const allHandsButtons = document.querySelectorAll(".buttons") // selects all the rock paper scissors buttons at the bottom
+const allHandsButtons = document.querySelectorAll(".buttons"); // selects all the rock paper scissors buttons at the bottom
+const resultDisplay = document.querySelector("#resultDisplay");
 
 // Adds a click event listener to all buttons
 allHandsButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        console.log(e.target.id)
+    // will get the player choice when they click the button
+    button.addEventListener("click", (playerChoice) => {
+        console.log(playerChoice.target.id);
+        drawRound()
     })
 })
 
@@ -12,4 +15,22 @@ function getRobotChoice() {
     const possibleChoices = ["rock", "paper", "scissors"];
     let randomChoice = Math.floor(Math.random() * possibleChoices.length);
     return possibleChoices[randomChoice];
+}
+
+// Makes the display show the result: player won
+function playerWonRound() {
+    resultDisplay.textContent = "Player won this round!";
+    resultDisplay.style.color = "green";
+}
+
+// Makes the display show the result: robot won
+function robotWonRound() {
+    resultDisplay.textContent = "Robot won this round!"
+    resultDisplay.style.color = "red";
+}
+
+// No winners this round
+function drawRound() {
+    resultDisplay.textContent = "Neither side won, it's a tie!"
+    resultDisplay.style.color = "black";
 }
