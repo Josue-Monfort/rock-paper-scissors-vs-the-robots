64d70533ff77fd60e56a56e6c6b79de8d968f463
josue <josuemonfortsantiago@gmail.com>
2022-04-27 12:31:22 -0300

    Made the speech bubble show the player and robot choice

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const allHandsButtons = document.querySelectorAll(".buttons"); // selects all th
 const resultDisplay = document.querySelector("#resultDisplay");
 const playerPointsDisplay = document.querySelector("#playerPointsDisplay"); // PLAYER POINTS
 const robotPointsDisplay = document.querySelector("#robotPointsDisplay"); // ROBOT POINTS
+const playerSpeechDisplay = document.querySelector("#normalBubbleImg"); // PLAYER SPEECH BUBBLE
+const robotSpeechDisplay = document.querySelector("#reverseBubbleImg"); // ROBOT SPEECH BUBBLE
 
 let playerPoints = 0;
 let robotPoints = 0;
@@ -22,55 +24,61 @@ function getRobotChoice() {
 }
 
 // Makes the display show the result: PLAYER WON
-function playerWonRound() {
+function playerWonRound(playerChoice, robotChoice) {
     playerPoints++;
     playerPointsDisplay.textContent = playerPoints;
     playerPointsDisplay.style.color = "green"
     robotPointsDisplay.style.color = "red"
     resultDisplay.textContent = "Player won this round!";
     resultDisplay.style.color = "green";
+    playerSpeechDisplay.setAttribute("src", `img/speech/speech-win-${playerChoice}.png`)
+    robotSpeechDisplay.setAttribute("src", `img/speech/speech-lose-${robotChoice}.png`)
 }
 
 // Makes the display show the result: ROBOT WON
-function robotWonRound() {
+function robotWonRound(playerChoice, robotChoice) {
     robotPoints++;
     robotPointsDisplay.textContent = robotPoints;
     playerPointsDisplay.style.color = "red"
     robotPointsDisplay.style.color = "green"
     resultDisplay.textContent = "Robot won this round!";
     resultDisplay.style.color = "red";
+    playerSpeechDisplay.setAttribute("src", `img/speech/speech-lose-${playerChoice}.png`)
+    robotSpeechDisplay.setAttribute("src", `img/speech/speech-win-${robotChoice}.png`)
 }
 
-// No winner this round
-function drawRound() {
+// No winners this round
+function drawRound(playerChoice, robotChoice) {
     resultDisplay.textContent = "Neither side won, it's a tie!";
     resultDisplay.style.color = "black";
     playerPointsDisplay.style.color = "black"
     robotPointsDisplay.style.color = "black"
+    playerSpeechDisplay.setAttribute("src", `img/speech/speech-lose-${playerChoice}.png`)
+    robotSpeechDisplay.setAttribute("src", `img/speech/speech-lose-${robotChoice}.png`)
 }
 
 // Will get the player input and compare with the robot choice
 // then it will evaluate and show the result.
 function getRoundResult(playerChoice, robotChoice) {
     if (playerChoice === robotChoice) {
-        return drawRound();
+        return drawRound(playerChoice, robotChoice);
     }
     if (playerChoice === "rock" && robotChoice === "scissors") {
-        return playerWonRound();
+        return playerWonRound(playerChoice, robotChoice);
     } 
     if (playerChoice === "rock" && robotChoice === "paper") {
-        return robotWonRound();
+        return robotWonRound(playerChoice, robotChoice);
     }
     if (playerChoice === "paper" && robotChoice === "scissors") {
-        return robotWonRound();
+        return robotWonRound(playerChoice, robotChoice);
     }
     if (playerChoice === "paper" && robotChoice === "rock") {
-        return playerWonRound();
+        return playerWonRound(playerChoice, robotChoice);
     }
     if (playerChoice === "scissors" && robotChoice === "paper") {
-        return playerWonRound();
+        return playerWonRound(playerChoice, robotChoice);
     }
     if (playerChoice === "scissors" && robotChoice === "rock") {
-        return robotWonRound();
+        return robotWonRound(playerChoice, robotChoice);
     }
 }
